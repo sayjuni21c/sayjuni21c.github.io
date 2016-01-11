716b58930ce6b5925f8c3751876c31da5ead5d49
sayjuni21c <sayjuni21c@gmail.com>
2016-01-12 00:11:18 +0900

    test

--- scripts/responsive.js
+++ scripts/responsive.js
@@ -12,3 +12,19 @@ window.onload = function() {
 
   sidebarButton.addEventListener("click", openSidebar, false);
 };
+
+window.fbAsyncInit = function() {
+    FB.init({
+      appId      : '175997306089238',
+      xfbml      : true,
+      version    : 'v2.5'
+    });
+  };
+
+  (function(d, s, id){
+     var js, fjs = d.getElementsByTagName(s)[0];
+     if (d.getElementById(id)) {return;}
+     js = d.createElement(s); js.id = id;
+     js.src = "//connect.facebook.net/en_US/sdk.js";
+     fjs.parentNode.insertBefore(js, fjs);
+   }(document, 'script', 'facebook-jssdk'));
